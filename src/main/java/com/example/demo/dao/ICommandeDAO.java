4d209233b9b744b8e09d4ade7320f30ec6eb1b79
YAHYA CHIBANE <80396129+Chibane690@users.noreply.github.com>
2021-03-26 22:35:27 +0100

    Update ICommandeDAO.java

--- src/main/java/com/example/demo/dao/ICommandeDAO.java
+++ src/main/java/com/example/demo/dao/ICommandeDAO.java
@@ -14,6 +14,6 @@ public interface ICommandeDAO {
 	
 	public int ValiderCommande(int idPanier, User c,boolean e,String local);
 	
-	public void ModifierCommande(int id,String local);
+	public void ModifierCommande(int id,String local); 
 	
 }
